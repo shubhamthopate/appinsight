f3eda7366a0e4a95921c1acfc52a30da1526e9af
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-19 16:52:42 +0530

    6th commit

--- yaml-pro/modules/appinsights/main.tf
+++ yaml-pro/modules/appinsights/main.tf
@@ -24,3 +24,11 @@ resource "azurerm_application_insights" "example" {
   application_type    = "web"
 }
 
+output "instrumentation_key" {
+  value = azurerm_application_insights.example.instrumentation_key
+}
+
+output "app_id" {
+  value = azurerm_application_insights.example.app_id
+  sensitive = true
+}
--- yaml-pro/modules/appinsights/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "instrumentation_key" {
-  value = azurerm_application_insights.example.instrumentation_key
-}
-
-output "app_id" {
-  value = azurerm_application_insights.example.app_id
-  sensitive = true
-}
