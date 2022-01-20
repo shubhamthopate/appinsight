63f4082bf7fc4365c4554f62bbbaa8dbdd3c77b7
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-20 09:47:23 +0530

    ddd

--- yaml-pro/modules/appservices/main.tf
+++ yaml-pro/modules/appservices/main.tf
@@ -17,6 +17,10 @@ resource "azurerm_app_service" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = ["${module.appseviceplan.appserviceplan-id}"]
+  tags = {
+    Dept: IT
+    Env: Dev
+  }
 
   site_config {
     dotnet_framework_version = "v4.0"
