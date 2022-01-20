00a1b2d0d7d1ca3d071cf99f0fba090f3c6e68d2
shubhamthopate <shubhamthopate9960@gmail.com>
2022-01-20 09:49:25 +0530

    efe

--- yaml-pro/modules/appservices/main.tf
+++ yaml-pro/modules/appservices/main.tf
@@ -18,8 +18,8 @@ resource "azurerm_app_service" "example" {
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = ["${module.appseviceplan.appserviceplan-id}"]
   tags = {
-    Dept: IT
-    Env: Dev
+    Dept: "IT"
+    Env: "Dev"
   }
 
   site_config {
